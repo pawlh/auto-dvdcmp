696a61c0b545f7efaa219311c19bda2bb5cc5ce3
Paul Hathaway <paul@pawlh.dev>
2024-10-08 20:35:14 -0600

    breakdown large functions into smaller units

--- ffmpeg.go
+++ ffmpeg.go
@@ -15,6 +15,7 @@ type MkvFile struct {
 	Duration time.Duration
 }
 
+// ScanMkvFiles scans the working directory for .mkv files and returns a slice of MkvFile structs
 func ScanMkvFiles(workingDirectory string) []MkvFile {
 	files, err := os.ReadDir(workingDirectory)
 	if err != nil {
@@ -23,36 +24,43 @@ func ScanMkvFiles(workingDirectory string) []MkvFile {
 
 	var mkvFiles []MkvFile
 	for _, file := range files {
-		if file.IsDir() {
-			continue
+		if isMkvFile(file) {
+			mkvFiles = append(mkvFiles, createMkvFile(workingDirectory, file))
 		}
-		if !strings.HasSuffix(file.Name(), ".mkv") {
-			continue
-		}
-
-		duration := getVideoDuration(filepath.Join(workingDirectory, file.Name()))
-
-		mkvFiles = append(mkvFiles, MkvFile{
-			Path:     file.Name(),
-			Duration: duration,
-		})
 	}
 
 	return mkvFiles
 }
 
+func isMkvFile(file os.DirEntry) bool {
+	return !file.IsDir() && strings.HasSuffix(file.Name(), ".mkv")
+}
+
+func createMkvFile(workingDirectory string, file os.DirEntry) MkvFile {
+	duration := getVideoDuration(filepath.Join(workingDirectory, file.Name()))
+	return MkvFile{
+		Path:     file.Name(),
+		Duration: duration,
+	}
+}
+
 func getVideoDuration(path string) time.Duration {
+	checkFfmpegInstalled()
+
+	cmd := exec.Command("ffmpeg", "-i", path)
+	output, _ := cmd.CombinedOutput()
+
+	return parseFfmpegDuration(output, path)
+}
+
+func checkFfmpegInstalled() {
 	_, err := exec.LookPath("ffmpeg")
 	if err != nil {
 		panic("ffmpeg is not installed")
 	}
+}
 
-	cmd := exec.Command("ffmpeg", "-i", path)
-	output, _ := cmd.CombinedOutput()
-	//if err != nil {
-	//	panic(err)
-	//}
-
+func parseFfmpegDuration(output []byte, path string) time.Duration {
 	durationRegEx := regexp.MustCompile(`Duration: (\d{2}):(\d{2}):(\d{2})`)
 	matches := durationRegEx.FindStringSubmatch(string(output))
 	if len(matches) != 4 {
